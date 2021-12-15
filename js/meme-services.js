0d229d8421aa466ba96b87e06227edd4e5a73659
Your Name <alexander.klashitsky@gmail.com>
2021-12-15 19:25:54 +0200

    fase 4

--- js/meme-services.js
+++ js/meme-services.js
@@ -1,4 +1,100 @@
 'use strict'
+
+var gImgs = [
+    {
+        id: 1,
+        url: 'img/meme-imgs (square)/1.jpg',
+        keyword: []
+    },
+
+    {
+        id: 2,
+        url: 'img/meme-imgs (square)/2.jpg',
+        keyword: []
+    },
+    {
+        id: 3,
+        url: 'img/meme-imgs (square)/3.jpg',
+        keyword: []
+    },
+    {
+        id: 4,
+        url: 'img/meme-imgs (square)/4.jpg',
+        keyword: []
+    },
+    {
+        id: 5,
+        url: 'img/meme-imgs (square)/5.jpg',
+        keyword: []
+    },
+    {
+        id: 6,
+        url: 'img/meme-imgs (square)/6.jpg',
+        keyword: []
+    },
+    {
+        id: 7,
+        url: 'img/meme-imgs (square)/7.jpg',
+        keyword: []
+    },
+    {
+        id: 8,
+        url: 'img/meme-imgs (square)/8.jpg',
+        keyword: []
+    },
+    {
+        id: 9,
+        url: 'img/meme-imgs (square)/9.jpg',
+        keyword: []
+    },
+    {
+        id: 10,
+        url: 'img/meme-imgs (square)/10.jpg',
+        keyword: []
+    },
+    {
+        id: 11,
+        url: 'img/meme-imgs (square)/11.jpg',
+        keyword: []
+    },
+    {
+        id: 12,
+        url: 'img/meme-imgs (square)/12.jpg',
+        keyword: []
+    },
+    {
+        id: 13,
+        url: 'img/meme-imgs (square)/13.jpg',
+        keyword: []
+    },
+    {
+        id: 14,
+        url: 'img/meme-imgs (square)/14.jpg',
+        keyword: []
+    },
+    {
+        id: 15,
+        url: 'img/meme-imgs (square)/15.jpg',
+        keyword: []
+    },
+    {
+        id: 16,
+        url: 'img/meme-imgs (square)/16.jpg',
+        keyword: []
+    },
+    {
+        id: 17,
+        url: 'img/meme-imgs (square)/17.jpg',
+        keyword: []
+    },
+    {
+        id: 18,
+        url: 'img/meme-imgs (square)/18.jpg',
+        keyword: [],
+    }
+
+
+]
 var gMeme = {
     pic: '',
     line1: '',
