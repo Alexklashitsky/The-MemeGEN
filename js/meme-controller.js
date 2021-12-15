0434f42482593c9222114b7d9cb03f4f078531f0
Your Name <alexander.klashitsky@gmail.com>
2021-12-15 12:32:35 +0200

    render meme

--- js/meme-controller.js
+++ js/meme-controller.js
@@ -1,1 +1,35 @@
 'use strict'
+var gCanvas
+var gCtx
+var pic = 'img1'
+var test = 'bla bla bla'
+
+
+gCanvas = document.querySelector('#my-canvas');
+gCtx = gCanvas.getContext('2d');
+
+// renderMeme(pic, test)
+
+
+
+
+
+function renderMeme(img, content) {
+    var content = content
+    //pic
+    var elImg = document.querySelector(`.${img}`);
+    console.log('elImg:', elImg);
+    gCtx.drawImage(elImg, 0, 0, gCanvas.width, gCanvas.height);
+    // console.log('txt:', test);
+
+
+    //txt
+    gCtx.textBaseline = 'middle';
+    gCtx.textAlign = 'center';
+    gCtx.font = '50px monospace';
+    gCtx.fillStyle = 'white';
+    gCtx.fillText(`${content}`, 200, 70);
+
+
+
+}
--- js/meme-services.js
+++ js/meme-services.js
@@ -1,1 +1,17 @@
 'use strict'
+var gMeme = {
+    pic: 'img1',
+    line1: 'bla bla bla'
+
+
+
+}
+
+
+getMeme()
+
+function getMeme() {
+    var pic = gMeme.pic
+    var line = gMeme.line1
+    renderMeme(pic, line)
+}
